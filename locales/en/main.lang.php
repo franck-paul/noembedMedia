d20b38f1a8a1449a5a88b48163176ff7ab73668b
franck <carnet.franck.paul@gmail.com>
2023-05-31 11:02:57 +0200

    l10n

---
+++ locales/en/main.lang.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @package Dotclear
+ *
+ * @copyright Olivier Meunier & Association Dotclear
+ * @copyright GPL-2.0-only
+ */
+#
+# DOT NOT MODIFY THIS FILE !
+#
+
+use Dotclear\Helper\L10n;
+
+L10n::$locales['Insert external media from Internet via noembed.com'] = '';
+L10n::$locales['noembed Media'] = '';
+L10n::$locales['External media'] = '';
+L10n::$locales['URL'] = '';
+L10n::$locales['Page URL:'] = '';
+L10n::$locales['URL cannot be empty.'] = '';
+L10n::$locales['Alignment'] = '';
+L10n::$locales['Media alignment:'] = '';
+L10n::$locales['None'] = '';
+L10n::$locales['Left'] = '';
+L10n::$locales['Right'] = '';
+L10n::$locales['Center'] = '';
+L10n::$locales['External media selector (via noembed.com)'] = '';
+L10n::$locales['Supported media services'] = '';
+L10n::$locales['See <a href="https://noembed.com/#supported-sites">this site</a> for supported services'] = '';
+L10n::$locales['Please enter the URL of the page containing the video you want to include in your post.'] = '';
+L10n::$locales['Ok'] = '';
+L10n::$locales['Media alignment'] = '';
+L10n::$locales['Media title'] = '';
+L10n::$locales['Title:'] = '';
+L10n::$locales['Insert'] = '';
+L10n::$locales['Cancel'] = '';
