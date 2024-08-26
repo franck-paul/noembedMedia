360ad3a24bbdf6a716a448e428d355deb696e57f
franck <carnet.franck.paul@gmail.com>
2024-08-26 08:48:46 +0200

    New release 4.3

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'noembed Media',
     'Insert external media from Internet via noembed.com',
     'Franck Paul and contributors',
-    '4.2',
+    '4.3',
     [
         'requires'    => [['core', '2.28']],
         'permissions' => 'My',
